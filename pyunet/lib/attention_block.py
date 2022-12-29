3fa8a23e683229eca5391efcb6859852c949bccb
Raphael Alampay <raphael.alampay@gmail.com>
2022-12-30 01:50:57 +0800

    checkpoint

---
+++ pyunet/lib/attention_block.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms.functional as TF
+
+class AttentionBlock(nn.Module):
+    def __init__(self, F_g, F_l, n_coefficients):
+        """
+        :param F_g: number of feature maps (channels) in previous layer
+        :param F_l: number of feature maps in corresponding encoder layer, transferred via skip connection
+        :param n_coefficients: number of learnable multi-dimensional attention coefficients
+        """
+        super(AttentionBlock, self).__init__()
+
+        self.W_gate = nn.Sequential(
+            nn.Conv2d(F_g, n_coefficients, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(n_coefficients)
+        )
+
+        self.W_x = nn.Sequential(
+            nn.Conv2d(F_l, n_coefficients, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(n_coefficients)
+        )
+
+        self.psi = nn.Sequential(
+            nn.Conv2d(n_coefficients, 1, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(1),
+            nn.Sigmoid()
+        )
+
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, gate, skip_connection):
+        """
+        :param gate: gating signal from previous layer
+        :param skip_connection: activation from corresponding encoder layer
+        :return: output activations
+        """
+        g1 = self.W_gate(gate)
+        x1 = self.W_x(skip_connection)
+        psi = self.relu(g1 + x1)
+        psi = self.psi(psi)
+        out = skip_connection * psi
+
+        return out
--- pyunet/lib/normalized_double_conv.py
+++ pyunet/lib/normalized_double_conv.py
@@ -1,19 +1,39 @@
 import torch
 import torch.nn as nn
 import torchvision.transforms.functional as TF
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
 from depthwise_seperable_conv import DepthwiseSeperableConv
 
+class AttentionConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True):
+        super(AttentionConv2d, self).__init__()
+        
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias)
+        self.attn = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=True)
+        self.sigmoid = nn.Sigmoid()
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        # calculate attention coefficients
+        x = self.conv(x)
+        attn_x = self.attn(x)
+        attn_x = self.relu(attn_x)
+        #x = torch.mul(x, attn_x)
+        x = x + attn_x
+
+        return x
+
 class NormalizedDoubleConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, padding=1):
         super(NormalizedDoubleConv, self).__init__()
 
         self.first_conv = DepthwiseSeperableConv(in_channels, out_channels)
         self.first_norm = nn.BatchNorm2d(out_channels)
-        self.first_actv = nn.ReLU6(inplace=True)
+        self.first_actv = nn.ReLU(inplace=True)
 
-        self.second_conv = DepthwiseSeperableConv(out_channels, out_channels)
-        self.second_norm = nn.BatchNorm2d(out_channels)
-        self.second_actv = nn.ReLU6(inplace=True)
+        self.attention = AttentionConv2d(out_channels, out_channels, 1)
 
         self.skip_conn = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
@@ -21,20 +41,21 @@ class NormalizedDoubleConv(nn.Module):
         )
 
     def forward(self, x):
-#        return self.conv(x)
+        # First convolution
+        x0 = self.first_conv(x)
+        x0 = self.first_norm(x0)
+        x0 = self.first_actv(x0)
+        #x0 = x0 * x
 
-        residual = x
+        # Second convolution
+        #x1 = self.second_conv(x0)
+        #x1 = self.second_norm(x1)
+        #x1 = self.second_actv(x1)
+        #x1 = x1 * x0
 
-        out = self.first_conv(x)
-        out = self.first_norm(out)
-        out = self.first_actv(out)
+        x0 = self.attention(x0)
 
-        out += self.skip_conn(residual)
+        # Residual
+        x0 += self.skip_conn(x)
 
-        out = self.second_conv(out)
-        out = self.second_norm(out)
-        out = self.second_actv(out)
-
-        out += self.skip_conn(residual)
-
-        return out
+        return x0
