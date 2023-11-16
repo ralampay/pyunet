ef95763fac7a9af3f1513ee2ffc145126c3b7ae2
Raphael Alampay <raphael.alampay@gmail.com>
2023-11-16 22:55:01 +0800

    replace TkAgg with Agg

--- pyunet/modules/sample_pair.py
+++ pyunet/modules/sample_pair.py
@@ -5,7 +5,7 @@ import cv2
 import numpy as np
 import random
 import matplotlib
-matplotlib.use('TkAgg')
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from utils import load_model_for_inference
 
